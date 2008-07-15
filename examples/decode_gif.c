7773151bd3162a53baed1fd267d0ec3da045152e
Sean Fox <dyntryx@gmail.com>
2008-07-15 16:02:13 +0000

    Decode example now includes local header so it's useful prior to installing the library
    svn path=/branches/dynis/libnsgif/; revision=4671

--- examples/decode_gif.c
+++ examples/decode_gif.c
@@ -24,7 +24,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <libnsgif.h>
+#include "../libnsgif.h"
 
 unsigned char *load_file(const char *path, size_t *data_size);
 void warning(const char *context, int code);
